6c695271e0f0bdea219965d0955610e72c7bd536
Nuttanee Knight <nuttaneeknight@gmail.com>
2023-03-09 13:49:39 -0500

    fixed the bugs

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,24 @@
-import React from 'react'
+import React from "react"
+import styled from "styled-components"
+import Contact from "./components/Contact"
 import Hero from './components/Hero'
 import Who from './components/Who'
 import Works from './components/Works'
-import Contact from './components/Contact'
 
 const Container = styled.div`
-
+height: 100vh;
+background-color: rebeccapurple
 `
 
 function App() {
+
   return (
-    <div>
+    <Container>
       <Hero />
       <Who />
-      <Works />
+      <Works /> 
       <Contact />
-    </div>
+    </Container>
   )
 }
 
