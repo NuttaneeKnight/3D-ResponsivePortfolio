2f32e9c576a22e192ea2f2cbbe233437e4bf9739
Nuttanee Knight <nuttaneeknight@gmail.com>
2023-03-09 17:27:00 -0500

    added img and bg color

--- src/App.jsx
+++ src/App.jsx
@@ -11,6 +11,8 @@ scroll-snap-type: y mandatory;
 scroll-behavior: smooth;
 overflow-y: auto;
 scrollbar-width: none;
+color: white;
+background: url("./img/bg.jpeg");
 &::-webkit-scrollbar{
   display: none;
 }
