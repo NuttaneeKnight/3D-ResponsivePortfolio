68164c68ee46eb8f8f7ac52f1e2c5409ea11c801
Nuttanee Knight <nuttaneeknight@gmail.com>
2023-03-09 11:14:12 -0500

    set all the 4 components in App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import Hero from './components/Hero'
+import Who from './components/Who'
+import Works from './components/Works'
+import Contact from './components/Contact'
+
+function App() {
+  return (
+    <div>
+      <Hero />
+      <Who />
+      <Works />
+      <Contact />
+    </div>
+  )
+}
+
+export default App
