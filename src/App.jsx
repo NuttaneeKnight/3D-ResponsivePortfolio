84f825d2394389ee7569d7c9b89e2adf85f28edf
Nuttanee Knight <nuttaneeknight@gmail.com>
2023-03-09 16:28:45 -0500

    got rid of the scroll bar on the side

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,13 @@ import Works from './components/Works'
 
 const Container = styled.div`
 height: 100vh;
-scroll-snap-type: mandatory;
+scroll-snap-type: y mandatory;
+scroll-behavior: smooth;
+overflow-y: auto;
+scrollbar-width: none;
+&::-webkit-scrollbar{
+  display: none;
+}
 `
 
 function App() {
