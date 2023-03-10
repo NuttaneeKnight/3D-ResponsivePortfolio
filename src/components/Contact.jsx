5a74eb1925630ff2444894a2cd7440fff899b79e
Nuttanee Knight <nuttaneeknight@gmail.com>
2023-03-10 14:30:40 -0500

    created a layout for Contact component

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,17 +1,41 @@
-import React from 'react'
-import styled from "styled-components"
+import React from "react";
+import styled from "styled-components";
 
 const Section = styled.div`
-height: 100vh;
-scroll-snap-align: center;
-`
+  height: 100vh;
+  scroll-snap-align: center;
+`;
+const Container = styled.div`
+  height: 100vh;
+  scroll-snap-align: center;
+`;
+const Left = styled.div`
+  height: 100vh;
+  scroll-snap-align: center;
+`;
+const Title = styled.h1`
+  height: 100vh;
+  scroll-snap-align: center;
+`;
+const Input = styled.input`
+  height: 100vh;
+  scroll-snap-align: center;
+`;
+const TextArea = styled.textarea`
+  height: 100vh;
+  scroll-snap-align: center;
+`;
+const Button = styled.button`
+  height: 100vh;
+  scroll-snap-align: center;
+`;
+const Right = styled.div`
+  height: 100vh;
+  scroll-snap-align: center;
+`;
 
 function Contact() {
-  return (
-    <Section>
-      Contact
-    </Section>
-  )
+  return <Section>Contact</Section>;
 }
 
-export default Contact
+export default Contact;
