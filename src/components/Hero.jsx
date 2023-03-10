25482967eb7c68098fa8a52f34923033a78f3ad5
Nuttanee Knight <nuttaneeknight@gmail.com>
2023-03-09 19:37:55 -0500

    fixed the layout for the navbar

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -7,6 +7,7 @@ const Section = styled.div`
   scroll-snap-align: center;
 `
 
+
 const Hero = () => {
   return (
     <Section>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,13 +2,72 @@ import React from 'react'
 import styled from 'styled-components'
 
 const Section = styled.div`
-height: 100vh;
-scroll-snap-align: center;
+  display: flex;
+  justify-content: center;
 `
+const Container = styled.div`
+  width: 1400px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 10px;
+`
+const Links = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 50px;
+`;
+
+const Logo = styled.img`
+  height: 50px;
+  margin: 20px
+`;
+
+const List = styled.ul`
+  display: flex;
+  gap: 20px;
+  list-style: none;
+
+  @media only screen and (max-width: 768px) {
+    display: none;
+  }
+`;
+const ListItem = styled.li`
+  
+`
+const Icon = styled.img`
+  
+`
+const Icons = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 20px;
+`
+
+const Button = styled.button`
+  
+`
+
 
 const Navbar = () => {
   return (
-    <Section>Navbar</Section>
+    <Section>
+      <Container>
+        <Links>
+          <Logo src='./img/logo.png' />
+          <List>
+            <ListItem>Home</ListItem>
+            <ListItem>Studio</ListItem>
+            <ListItem>Works</ListItem>
+            <ListItem>Contact</ListItem>
+          </List>
+        </Links>
+        <Icons>
+          <Icon src='./img/search.png' />
+          <Button>Hire Now</Button>
+        </Icons>
+      </Container>
+    </Section>
   )
 }
 
