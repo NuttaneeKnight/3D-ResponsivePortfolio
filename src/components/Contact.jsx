194aa620f0333e908550769d27a56edea20e316d
Nuttanee Knight <nuttaneeknight@gmail.com>
2023-03-10 17:40:16 -0500

    debug the typo on the from styled component

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -6,43 +6,52 @@ const Section = styled.div`
   scroll-snap-align: center;
 `;
 const Container = styled.div`
-  height: 100vh;
-  scroll-snap-align: center;
+  width: 100%;
+  height: 100%;
+  display: flex;
+  justify-content: space-between;
+  gap: 50px;
 `;
 const Left = styled.div`
-  height: 100vh;
-  scroll-snap-align: center;
+  flex: 1;
+  display: flex;
+  align-items: center;
+  justify-content: center;
 `;
 const Title = styled.h1`
-  height: 100vh;
-  scroll-snap-align: center;
+  font-weight: 200;
 `;
-const Form = styled.input`
-  height: 100vh;
-  scroll-snap-align: center;
+const Form = styled.form`
+  width: 500px;
+  display: flex;
+  flex-direction: column;
+  gap: 25px;
 `;
 const Input = styled.input`
-  height: 100vh;
-  scroll-snap-align: center;
+  padding: 20px;
+  background-color: lightgray;
 `;
 const TextArea = styled.textarea`
-  height: 100vh;
-  scroll-snap-align: center;
+  padding: 20px;
 `;
-const Button = styled.button`
-  height: 100vh;
-  scroll-snap-align: center;
-`;
+const Button = styled.button``;
 const Right = styled.div`
-  height: 100vh;
-  scroll-snap-align: center;
+  flex: 1;
 `;
 
 const Contact = () => {
   return (
     <Section>
       <Container>
-        <Left></Left>
+        <Left>
+          <Form>
+            <Title>Contact</Title>
+            <Input placeholder="Name" />
+            <Input placeholder="Email" />
+            <TextArea placeholder="Message" />
+            <Button>Send</Button>
+          </Form>
+        </Left>
         <Right></Right>
       </Container>
     </Section>
